e82516509d2c70c73fffd2488be45d9798d029da
Rohit Singh <rohitsingh4073@gmail.com>
2026-09-10 15:57:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // BACKEND URL
 // ============================================================
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://cooperative-market-v2.onrender.com";
 
 // ============================================================
 // CURRENT LOGGED-IN USER
