4c3dbe01653f3d843650b5d4f5369fd261899846
Rohit Singh <rohitsingh4073@gmail.com>
2026-09-10 22:10:37 +0530

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1006,4 +1006,5 @@ if __name__ == "__main__":
     print("Uploads folder: uploads/")
     print()
 
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
