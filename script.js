8dc03c12049f959cf5b576da74bddb7f92a9bc85
Rohit Singh <rohitsingh4073@gmail.com>
2026-09-10 22:13:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // BACKEND URL
 // ============================================================
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = window.location.origin;
 
 // ============================================================
 // CURRENT LOGGED-IN USER
